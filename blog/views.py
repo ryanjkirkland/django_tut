acb0c739f13394e0979fdb5cbe06bf84773b3d14
Ryan Kirkland <ryankirkland@Ryans-MacBook-Pro.local>
2020-01-12 12:05:10 -0700

    added post model and updated view to update dynamically based on database changes

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'August 27th, 2019'
-	},
-	{
-		'author': 'JaneDoe',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'August 29th, 2019'
-	}
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
