f1c7669a5149e39bf4c62021aa7b63f8141347eb
Ryan Kirkland <ryankirkland@Ryans-MacBook-Pro.local>
2020-01-19 16:20:39 -0700

    update allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'or4p&m=ub6-!5ux_*7=)k(zvsb6$f$+nk78jk9_5y_$%xc_ob9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangotutapprk.herokuapp.com']
 
 
 # Application definition
