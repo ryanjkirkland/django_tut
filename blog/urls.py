50983ca58b380338163b2bfb92051779083861b9
Ryan Kirkland <ryankirkland@Ryans-MacBook-Pro.local>
2020-01-19 16:23:16 -0700

    chartjs crap

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, LineChartJSONView
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
