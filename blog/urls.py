f03657b8052cc493181b6f37f1324e91c88bc7e7
Ryan Kirkland <ryankirkland@Ryans-MacBook-Pro.local>
2020-01-19 15:37:31 -0700

    testing charts js

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, LineChartJSONView
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='blog-home'),
+	path('', PostListView.as_view(), name='blog-home'),
+	path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 	path('about/', views.about, name='blog-about'),
+	path('line_chart/', LineChartJSONView.as_view(), name='line-chart'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Post
+
+# CHARTJS SHIT
+
+from random import randint
+from chartjs.views.lines import BaseLineChartView
 
 def home(request):
 	context = {
@@ -7,5 +13,34 @@ def home(request):
 	}
 	return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = Post
+
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+
+line_chart = TemplateView.as_view(template_name='line_chart.html')
+line_chart_json = LineChartJSONView.as_view()
